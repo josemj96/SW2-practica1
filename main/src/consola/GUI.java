4a821aaa2f9ea84f7f755aba3dd377da206f69e1
willypc98 <willpc98@gmail.com>
2021-03-27 12:02:51 +0100

    Cmabios varios y primera versión de GUI (faltan cosas)

--- main/src/consola/GUI.java
+++
@@ -1,4 +0,0 @@
-package consola;
-
-public class GUI {
-}
---
+++ main/src/xml/GUI.java
@@ -0,0 +1,35 @@
+package xml;
+
+import clases.Producto;
+import clases.Marca;
+
+import java.util.ArrayList;
+
+public class GUI {
+    Marshalling mrs = new Marshalling();
+    CheckXSD vXSD = new CheckXSD();
+    CheckDTD vDTD = new CheckDTD();
+    VisualizadorObjetos vO = new VisualizadorObjetos();
+    CrearObjetos co= new CrearObjetos();
+    XQuery XQ= new XQuery();
+    XPathBuscar XPS= new XPathBuscar();
+    ArrayList<Producto> productoArrayList = new ArrayList();
+    Producto producto;
+    Marca marca = new Marca();
+
+    private void crearXMLMarca(String Fichero, Marca marca){
+        mrs.crearXMLMarca(Fichero, marca);
+    }
+    private void crearXMLProducto(String nombreProducto, String Fichero){
+        mrs.crearXMLProducto(Fichero,vO.buscarProducto(nombreProducto,marca));
+    }
+    private Producto importarObjetoProducto(String Fichero){
+        Producto producto = mrs.importarObjetoProducto(Fichero);
+        return producto;
+    }
+    private Marca importarObjetoMarca(String Fichero){
+        Marca marca = mrs.importarObjetoMarca(Fichero);
+        return marca;
+    }
+
+}
