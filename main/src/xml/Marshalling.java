77589671553ca4f2505ca2924f4550889ce9d460
willypc98 <willpc98@gmail.com>
2021-03-27 10:38:35 +0100

    Marshalling y Unmarshalling de Producto

--- main/src/xml/Marshalling.java
+++ main/src/xml/Marshalling.java
@@ -66,4 +66,43 @@ public class Marshalling {
         }
         return marca;
     }
+
+    protected void crearXMLProducto(String Fichero, Producto producto) {
+            try {
+                FileWriter fichero = null;
+                File file = new File("main/files/xml/" + Fichero);
+                comprobarFichero(file);
+            } catch (Exception ex) {
+
+                System.out.println("La consulta ha fallado");
+            }
+            try {
+                FileWriter fichero = null;
+                JAXBContext context = JAXBContext.newInstance(Producto.class);
+                Marshaller m = context.createMarshaller();
+                m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                m.marshal(producto, fichero);
+                fichero.close();
+            } catch (Exception ex) {
+                Logger.getLogger(Marshalling.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    protected Producto importarObjetoProducto(String Fichero){
+        Producto producto= new Producto();
+        try {
+
+            File file = new File("main/files/xml/" + Fichero);
+            JAXBContext jaxbContext = JAXBContext.newInstance(Producto.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            producto = (Producto) jaxbUnmarshaller.unmarshal(file);
+
+
+
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return producto;
+    }
 }
