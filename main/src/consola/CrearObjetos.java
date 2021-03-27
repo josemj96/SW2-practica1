e75e4febd90a41831683d7393c58d2853b55e950
willypc98 <willpc98@gmail.com>
2021-03-27 11:21:20 +0100

    Creación de la marca y el Producto usando un Scanner

--- main/src/consola/CrearObjetos.java
+++ main/src/consola/CrearObjetos.java
@@ -1,4 +1,42 @@
 package consola;
 
+import clases.Producto;
+import clases.Marca;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CrearObjetos {
+
+    protected Producto crearProducto() {
+        Scanner scanner = new Scanner(System.in);
+        Producto producto = new Producto();
+        System.out.println("Introduce el nombre del producto");
+        producto.setNombre(scanner.nextLine());
+        System.out.println("Introduce la fecha de publicación del producto separando los elementos con /");
+        producto.setFechaDePublicacion(scanner.nextLine());
+        System.out.println("Introduce el tipo de producto");
+        producto.setTipo(scanner.nextLine());
+        System.out.println("Introduce la descripción del producto");
+        producto.setDescripcion(scanner.nextLine());
+        System.out.println("Introduce el precio de la producto");
+        producto.setPrecio(Double.parseDouble(scanner.nextLine()));
+
+        return producto;
+    }
+
+    protected Marca crearMarca(ArrayList<Producto> productos) {
+        Scanner scanner = new Scanner(System.in);
+        Marca marca = new Marca();
+        if (productos.isEmpty()) {
+
+            System.out.println("Debes crear los productos antes");
+            marca = null;
+        } else {
+            System.out.println("Introduce el nombre de la marca");
+            marca.setNombre(scanner.nextLine());
+            System.out.println("Se introducirán las productos en la Marca");
+            marca.setProductos(productos);
+        }
+        return marca;
+    }
 }
